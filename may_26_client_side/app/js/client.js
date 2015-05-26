05cb2439fc84a7c8ca24025deec0a240c92ff205
toastynerd <tylermorgan86@gmail.com>
2015-05-26 15:59:27 -0700

    done with basic angular

--- may_26_client_side/app/js/client.js
+++ may_26_client_side/app/js/client.js
@@ -1,20 +1,5 @@
 'use strict';
 
-var greet = require('./greet');
-document.write(greet());
-var noteList = document.getElementById('notelist');
+require('angular/angular');
 
-var request = require('superagent');
-
-  request
-  .get('/api/notes')
-  .end(function(err, res) {
-    if (err) return console.log(err);
-    var notes = JSON.parse(res.text);
-
-    notes.forEach(function(note) {
-      var noteEl = document.createElement('li');
-      noteEl.innerHTML = note.noteBody;
-      noteList.appendChild(noteEl);
-    });
-  });
+var notesApp = angular.module('notesApp', []);
