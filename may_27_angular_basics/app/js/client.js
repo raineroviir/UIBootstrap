64e8eec0d44e93d7ecfc3daf58f4021dd3e9af42
toastynerd <tylermorgan86@gmail.com>
2015-05-27 16:06:31 -0700

    finish note crud

--- may_27_angular_basics/app/js/client.js
+++ may_27_angular_basics/app/js/client.js
@@ -4,18 +4,4 @@ require('angular/angular');
 
 var notesApp = angular.module('notesApp', []);
 
-notesApp.controller('notesController',['$scope', function($scope) {
-  $scope.greeting = 'hello world!';  
-  $scope.displayGreeting = function() {
-    alert($scope.greeting);
-  };
-}]);
-
-
-notesApp.controller('anotherController', ['$scope', function($scope) {
- $scope.greeting = 'another greeting';
-}]);
-
-notesApp.controller('oneMoreController', ['$scope', function($scope) {
-
-}]);
+require('./notes/controllers/notes_controller')(notesApp);
