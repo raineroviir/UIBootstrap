54fe065c78678007ae667501d32462ce4aa63b9a
toastynerd <tylermorgan86@gmail.com>
2015-05-27 14:01:02 -0700

    basics done

--- may_27_angular_basics/app/js/client.js
+++ may_27_angular_basics/app/js/client.js
@@ -3,3 +3,19 @@
 require('angular/angular');
 
 var notesApp = angular.module('notesApp', []);
+
+notesApp.controller('notesController',['$scope', function($scope) {
+  $scope.greeting = 'hello world!';  
+  $scope.displayGreeting = function() {
+    alert($scope.greeting);
+  };
+}]);
+
+
+notesApp.controller('anotherController', ['$scope', function($scope) {
+ $scope.greeting = 'another greeting';
+}]);
+
+notesApp.controller('oneMoreController', ['$scope', function($scope) {
+
+}]);
