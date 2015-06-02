354f8821b68287f3f8d9f537450c775a8784aee7
toastynerd <tylermorgan86@gmail.com>
2015-06-02 12:57:06 -0700

    start of day

---
+++ jun_2_services_directives/Gruntfile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.initConfig({
+    webpack: {
+      client: {
+        entry: __dirname + '/app/js/client.js',
+        output: {
+          path: 'build/',
+          file: 'bundle.js'
+        }
+      },
+      test: {
+        entry: __dirname + '/test/client/test.js',
+        output: {
+          path: 'test/client/',
+          file: 'test_bundle.js'
+        }
+      },
+      karma_test: {
+        entry: __dirname + '/test/karma_tests/test_entry.js',
+        output: {
+          path: 'test/karma_tests/',
+          file: 'bundle.js'
+        }
+      }
+    },
+
+    copy: {
+      html: {
+        cwd: 'app/',
+        expand: true,
+        flatten: false,
+        src: '**/*.html',
+        dest: 'build/',
+        filter: 'isFile'
+      }
+    },
+
+    clean: {
+      dev: {
+        src: 'build/'
+      }
+    }
+  });
+
+  grunt.registerTask('build:dev', ['webpack:client', 'copy:html']);
+  grunt.registerTask('build', ['build:dev']);
+  grunt.registerTask('default', ['build']);
+};
